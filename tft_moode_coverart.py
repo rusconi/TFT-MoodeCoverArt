66000ee040ae1d43a2bb449125dc6bf8c97558d8
pachisb <pachisb@gmail.com>
2021-11-29 19:48:52 +0100

    Initialization improvements and speed up; resize images only once, after loading them; volume bar is shown briefly after changes, if overlay config variable value is 2

--- tft_moode_coverart.py
+++ tft_moode_coverart.py
@@ -46,65 +46,36 @@ if os.path.exists(confile):
         MODE = displayConf["mode"]
         BLANK = displayConf["blank"]
 
-
-# Standard SPI connections for ST7789
-# Create ST7789 LCD display class
-if MODE == 3:
-    disp = ST7789.ST7789(
-        port=0,
-        cs=ST7789.BG_SPI_CS_FRONT,  # GPIO 8, Physical pin 24
-        dc=9,
-        rst=22,
-        backlight=13,
-        mode=3,
-        rotation=0,
-        spi_speed_hz=80 * 1000 * 1000
-    )
-else:
-    disp = ST7789.ST7789(
-        port=0,
-        cs=ST7789.BG_SPI_CS_FRONT,  # GPIO 8, Physical pin 24
-        dc=9,
-        backlight=13,
-        spi_speed_hz=80 * 1000 * 1000
-    )
-
-# FIXME: this prevents MPD from detecting buttons state, so we cannot use it
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(6, GPIO.IN)
-# GPIO.setup(24, GPIO.IN)
-
 WIDTH = 240
 HEIGHT = 240
 font_s = ImageFont.truetype(script_path + "/fonts/Roboto-Medium.ttf", 20)
 font_m = ImageFont.truetype(script_path + "/fonts/Roboto-Medium.ttf", 24)
 font_l = ImageFont.truetype(script_path + "/fonts/Roboto-Medium.ttf", 28)
 
-img = Image.new("RGB", (240, 240), color=(0, 0, 0, 25))
+img = Image.new("RGB", (WIDTH, HEIGHT), color=(0, 0, 0, 25))
 draw = ImageDraw.Draw(img, "RGBA")
 
-play_icons = Image.open(script_path + "/images/controls-play.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
-play_icons_dark = Image.open(script_path + "/images/controls-play-dark.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
+play_icons = Image.open(script_path + "/images/controls-play.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGBA")
+play_icons_dark = Image.open(script_path + "/images/controls-play-dark.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGBA")
 
-pause_icons = Image.open(script_path + "/images/controls-pause.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
-pause_icons_dark = Image.open(script_path + "/images/controls-pause-dark.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
+pause_icons = Image.open(script_path + "/images/controls-pause.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGBA")
+pause_icons_dark = Image.open(script_path + "/images/controls-pause-dark.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGBA")
 
-vol_icons = Image.open(script_path + "/images/controls-vol.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
-vol_icons_dark = Image.open(script_path + "/images/controls-vol-dark.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
+vol_icons = Image.open(script_path + "/images/controls-vol.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGBA")
+vol_icons_dark = Image.open(script_path + "/images/controls-vol-dark.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGBA")
 
-df_back = Image.open(script_path + "/images/default-cover-v6.jpg")  # resized later
-bt_back = Image.open(script_path + "/images/bta.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
-ap_back = Image.open(script_path + "/images/airplay.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
-sp_back = Image.open(script_path + "/images/spotify.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
-sq_back = Image.open(script_path + "/images/squeeze.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
-jp_back = Image.open(script_path + "/images/jack.png").resize((240,240), resample=Image.LANCZOS).convert("RGBA")
+df_back = Image.open(script_path + "/images/default-cover-v6.jpg").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGB")
+bt_back = Image.open(script_path + "/images/bta.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGB")
+ap_back = Image.open(script_path + "/images/airplay.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGB")
+sp_back = Image.open(script_path + "/images/spotify.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGB")
+sq_back = Image.open(script_path + "/images/squeeze.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGB")
+jp_back = Image.open(script_path + "/images/jack.png").resize((WIDTH, HEIGHT), resample=Image.LANCZOS).convert("RGB")
 
 
 def is_service_active(service):
 
     waiting = True
     count = 0
-    active = False
 
     while (waiting == True):
 
@@ -114,15 +85,15 @@ def is_service_active(service):
 
         if stat == "active":
             waiting = False
-            active = True
+            return True
 
-        if count > 14:
+        if count > 29:
             waiting = False
 
         count += 1
-        time.sleep(2)
+        time.sleep(1)
 
-    return active
+    return False
 
 
 def get_moode_metadata(filename):
@@ -180,7 +151,7 @@ def get_cover(metaDict):
                 rc = "/var/local/www/" + metaDict["coverurl"]
                 if os.path.exists(rc):
                     if rc != "/var/local/www/images/default-cover-v6.svg":
-                        result = Image.open(rc)
+                        result = Image.open(rc).resize((WIDTH, HEIGHT), Image.LANCZOS).convert("RGB")
         elif metaDict["source"] == "bluetooth":
             result = bt_back
         elif metaDict["source"] == "airplay":
@@ -200,30 +171,26 @@ def get_cover(metaDict):
                     if EMBCOVERPRIO == 1:
                         mf = MediaFile(fp)
                         if mf.art:
-                            result = Image.open(BytesIO(mf.art))
+                            result = Image.open(BytesIO(mf.art)).resize((WIDTH, HEIGHT), Image.LANCZOS).convert("RGB")
                             coverok = True
                     if not coverok:
                         for it in covers:
                             cp = os.path.dirname(fp) + "/" + it
                             if os.path.exists(cp):
-                                result = Image.open(cp)
+                                result = Image.open(cp).resize((WIDTH, HEIGHT), Image.LANCZOS).convert("RGB")
                                 coverok = True
                                 break
                     if not coverok and EMBCOVERPRIO == 0:
                         mf = MediaFile(fp)
                         if mf.art:
-                            result = Image.open(BytesIO(mf.art))
+                            result = Image.open(BytesIO(mf.art)).resize((WIDTH, HEIGHT), Image.LANCZOS).convert("RGB")
 
-    # return filename and image for cover (tuple)
-    return result
+    # return normal and blurred image for cover (tuple)
+    resultgauss = result.filter(ImageFilter.GaussianBlur).convert("RGB")
+    return result, resultgauss
 
 
 def main():
-
-    # Initialize display
-    disp.begin()
-
-    disp.set_backlight(True)
 
     filename = "/var/local/www/currentsong.txt"
 
@@ -238,13 +205,41 @@ def main():
     title_top = 105
     volume_top = 184
     time_top = 222
-    act_mpd = is_service_active("mpd")
     cover = None
-    oldcover = None
+    covergauss = None
     oldcovername = ""
     vol = 0
     oldvol = -1
     volchanged = 0
+
+    act_mpd = is_service_active("mpd")
+
+    # Standard SPI connections for ST7789
+    # Create and initialize ST7789 LCD display class (also turns the backlight on)
+    if MODE == 3:
+        disp = ST7789.ST7789(
+            port=0,
+            cs=ST7789.BG_SPI_CS_FRONT,  # GPIO 8, Physical pin 24
+            dc=9,
+            rst=22,
+            backlight=13,
+            mode=3,
+            rotation=0,
+            spi_speed_hz=80 * 1000 * 1000
+        )
+    else:
+        disp = ST7789.ST7789(
+            port=0,
+            cs=ST7789.BG_SPI_CS_FRONT,  # GPIO 8, Physical pin 24
+            dc=9,
+            backlight=13,
+            spi_speed_hz=80 * 1000 * 1000
+        )
+
+    # FIXME: this prevents MPD from detecting buttons state, so we cannot use it
+    # GPIO.setmode(GPIO.BCM)
+    # GPIO.setup(6, GPIO.IN)
+    # GPIO.setup(24, GPIO.IN)
 
     if act_mpd == True:
         while True:
@@ -252,11 +247,11 @@ def main():
             try:
                 client.connect()  # use MPD_HOST/MPD_PORT
             except KeyboardInterrupt:
+                print("Keyboard interrupt, exiting!", file = sys.stderr)
                 raise
             except BaseException as e:
                 print(e, file = sys.stderr)
                 time.sleep(2)
-                pass
             else:
                 try:
                     moode_meta = get_moode_metadata(filename)
@@ -290,33 +285,22 @@ def main():
                     # print(mpd_status)
                     # print()
 
+                    state = mpd_status["state"] if "state" in mpd_status else None
+
                     # avoid re-reading the previous image again
                     covername = get_cover_filepath(moode_meta)
                     if (covername != oldcovername):
                         oldcovername = covername
-                        # print("reading cover for: ", covername)
-                        cover = get_cover(moode_meta)
-                        oldcover = cover
+                        # print("Reading cover for: ", covername)
+                        cover, covergauss = get_cover(moode_meta)
                         im_stat = ImageStat.Stat(cover)
                         im_mean = im_stat.mean
                         mn = mean(im_mean)
-                    else:
-                        cover = oldcover
 
-                    if TEXT == 0 or ("state" in mpd_status and mpd_status["state"] != "play"):
-                        img.paste(cover.resize((WIDTH,HEIGHT), Image.LANCZOS).convert("RGB"))
+                    if TEXT == 0 or (state and (state != "play")):
+                        img.paste(cover)
                     else:
-                        img.paste(cover.resize((WIDTH,HEIGHT), Image.LANCZOS).filter(ImageFilter.GaussianBlur).convert("RGB"))
-
-                    if "state" in mpd_status:
-                        if (mpd_status["state"] == "stop") and (BLANK != 0):
-                            if ss < BLANK:
-                                ss = ss + 1
-                            else:
-                                disp.set_backlight(False)
-                        else:
-                            ss = 0
-                            disp.set_backlight(True)
+                        img.paste(covergauss)
 
                     # txt_col = (255-int(im_mean[0]), 255-int(im_mean[1]), 255-int(im_mean[2]))
                     txt_col = (255,255,255)
@@ -335,9 +319,9 @@ def main():
                     if (moode_meta["source"] == "library") or (moode_meta["source"] == "radio"):
 
                         if (OVERLAY > 0):
-                            if "state" in mpd_status:
+                            if state:
                                 if OVERLAY == 3 or OVERLAY == 2:
-                                    if mpd_status["state"] != "play":
+                                    if state != "play":
                                         if dark is False:
                                             img.paste(play_icons, (0,0), play_icons)
                                         else:
@@ -355,7 +339,7 @@ def main():
                                         volchanged = 5
                                     oldvol = vol
 
-                                if ((OVERLAY == 3 or OVERLAY == 1) and "state" in mpd_status and mpd_status["state"] == "play") or (OVERLAY == 2 and volchanged > 0):
+                                if ((OVERLAY == 3 or OVERLAY == 1) and state == "play") or (OVERLAY == 2 and volchanged > 0):
                                     if volchanged > 0:
                                         volchanged -= 1
 
@@ -371,7 +355,7 @@ def main():
                             else:
                                 img.paste(play_icons, (0,0), play_icons)
 
-                        if TIMEBAR == 1 and ("state" in mpd_status and mpd_status["state"] == "play"):
+                        if TIMEBAR == 1 and state == "play":
                             if "elapsed" in mpd_status:
                                 el_time = int(float(mpd_status["elapsed"]))
                                 if "duration" in mpd_status:
@@ -380,7 +364,7 @@ def main():
                                     draw.rectangle((5, time_top, WIDTH-5, time_top + 12), (255,255,255,145))
                                     draw.rectangle((5, time_top, dur_x, time_top + 12), bar_col)
 
-                        if TEXT == 1 and ("state" in mpd_status and mpd_status["state"] == "play"):
+                        if TEXT == 1 and state == "play":
                             if "artist" in moode_meta:
                                 w1, y1 = draw.textsize(moode_meta["artist"], font_m)
                                 x1 = x1-20
@@ -424,38 +408,51 @@ def main():
                                 draw.text((x3+SHADOW, y3+SHADOW), txt, font=font_l, fill=shd_col)
                             draw.text((x3, y3), txt, font=font_l, fill=txt_col, spacing=6, align="center")
 
-                    disp.display(img)
-
                     if c == 0:
+
+                        # NOTE: initialization is unnecesary for ST7789, the class does it when created
+                        # Initialize display
+                        # disp.set_backlight(True)
+
                         # im7 = img.save(script_path + "/dump.jpg")
                         c += 1
 
-                    if ("state" in mpd_status and mpd_status["state"] == "play"):
+                    disp.display(img)
+
+                    if state:
+                        if state == "stop" and BLANK != 0:
+                            if ss < BLANK:
+                                ss += 1
+                            else:
+                                disp.set_backlight(False)
+                        else:
+                            ss = 0
+                            disp.set_backlight(True)
+
+                    if state == "play":
                         time.sleep(0.1)
                     else:
                         time.sleep(0.5)
 
                 except KeyboardInterrupt:
-                    raise
+                    print("Keyboard interrupt, exiting!", file = sys.stderr)
+                    disp.set_backlight(False)
+                    # disp.reset()
+                    break
                 except BaseException as e:
                     print(e, file = sys.stderr)
                     time.sleep(2)
 
     else:  # act_mpd != True:
 
-        draw.rectangle((0,0,240,240), fill=(0,0,0))
+        draw.rectangle((0, 0, WIDTH, HEIGHT), fill=(0, 0, 0))
         txt = "MPD not Active!\nEnsure MPD is running\nThen restart script"
         mlw, mlh = draw.multiline_textsize(txt, font=font_m, spacing=4)
-        draw.multiline_text(((WIDTH-mlw)//2, 20), txt, fill=(255,255,255), font=font_m, spacing=4, align="center")
+        draw.multiline_text(((WIDTH-mlw)//2, 20), txt, fill=(255, 255, 255), font=font_m, spacing=4, align="center")
         disp.display(img)
 
 
 if __name__ == "__main__":
 
-    try:
-        main()
-    except KeyboardInterrupt:
-        disp.reset()
-        disp.set_backlight(False)
-        pass
+    main()
 
